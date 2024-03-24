35e09df88af5f7ca34a17028ae5564e8cd042426
John Mwangi <mail.mwangi@gmail.com>
2024-03-24 23:55:20 +0300

    Documentation update

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from src.scrape_website import (
 
 # TODO: implement asynchronous (Puppeteer, Playwright)
 def main(url: str, **kwargs):
+    """
+    Args:
+    ---
+    - kwargs: kwargs to pass to `soup.find_all()`
+    """
     page_name = url.split("/")[-2]
 
     is_html_parsed = page_parsed(page_name, content_type=ContentType.HTML)
