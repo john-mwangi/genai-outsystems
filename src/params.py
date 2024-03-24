05ea8e292a78b3276c54e61015a15b3d4094809e
John Mwangi <mail.mwangi@gmail.com>
2024-03-24 23:24:44 +0300

    Update cursor position

--- src/params.py
+++ src/params.py
@@ -11,4 +11,4 @@ htmls_dir = ROOT_DIR / "files/html_files"
 texts_dir = ROOT_DIR / "files/txt_files"
 
 # TODO: Make this dynamic
-right_click_pst = {"x": 984, "y": 204}
+right_click_pst = {"x": 990, "y": 215}
